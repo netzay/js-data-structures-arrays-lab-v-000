47167b3aae6d088b7bd892874607438393e0f5bd
Ally Netzel <netzelally@gmail.com>
2018-11-23 02:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function appendDriver(name){
 }
 
 function prependDriver(name){
-  const newDriver = [...drivers.slice(), 'b'];
+  const newDriver = [...drivers.slice(0,0), 'Arnold'];
   return newDriver;
 }
